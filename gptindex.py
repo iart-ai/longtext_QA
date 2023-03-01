cc486b9d80af99764e596ea209637fc0d258e329
AlexW <429774648@qq.com>
2023-03-01 11:09:17 +0800

    first commit

---
+++ gptindex.py
@@ -0,0 +1,208 @@
+from llama_index import SimpleDirectoryReader, GPTListIndex,GPTSimpleVectorIndex,LLMPredictor,PromptHelper
+from langchain import OpenAI
+import sys
+import os
+
+import string
+import re
+
+import openai
+import urllib.request, json
+from serpapi import GoogleSearch
+from html2text import html2arti,repeat_keyword
+
+os.environ["OPENAI_API_KEY"]='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'
+
+serpapi_key = "" #get one from https://serpapi.com , first few requests are free!
+
+prompt = ['''Question: Who lived longer, Muhammad Ali or Alan Turing?
+Are follow up questions needed here: Yes.
+Follow up: How old was Muhammad Ali when he died?
+Intermediate answer: Muhammad Ali was 74 years old when he died.
+Follow up: How old was Alan Turing when he died?
+Intermediate answer: Alan Turing was 41 years old when he died.
+So the final answer is: Muhammad Ali 
+
+Question: When was the founder of craigslist born?
+Are follow up questions needed here: Yes.
+Follow up: Who was the founder of craigslist?
+Intermediate answer: Craigslist was founded by Craig Newmark.
+Follow up: When was Craig Newmark born?
+Intermediate answer: Craig Newmark was born on December 6, 1952.
+So the final answer is: December 6, 1952
+
+Question: Who was the maternal grandfather of George Washington?
+Are follow up questions needed here: Yes.
+Follow up: Who was the mother of George Washington?
+Intermediate answer: The mother of George Washington was Mary Ball Washington.
+Follow up: Who was the father of Mary Ball Washington?
+Intermediate answer: The father of Mary Ball Washington was Joseph Ball.
+So the final answer is: Joseph Ball 
+
+Question: Are both the directors of Jaws and Casino Royale from the same country? 
+Are follow up questions needed here: Yes. 
+Follow up: Who is the director of Jaws? 
+Intermediate Answer: The director of Jaws is Steven Spielberg. 
+Follow up: Where is Steven Spielberg from? 
+Intermediate Answer: The United States. 
+Follow up: Who is the director of Casino Royale? 
+Intermediate Answer: The director of Casino Royale is Martin Campbell. 
+Follow up: Where is Martin Campbell from? 
+Intermediate Answer: New Zealand. 
+So the final answer is: No
+
+Question: ''',
+'''
+Are follow up questions needed here:''', ]
+
+def construct_index(directory_path):
+    max_input_size = 4096
+    num_outputs= 256
+    max_chunnk_overlap = 20
+    chunk_size_limit = 600
+
+    prompt_helper = PromptHelper(max_input_size,num_outputs,max_chunnk_overlap,chunk_size_limit)
+    llm_predictor = LLMPredictor(llm=OpenAI(temperature=0, model_name="text-ada-001", max_tokens=num_outputs))
+
+    documents = SimpleDirectoryReader(directory_path).load_data()
+
+    index = GPTSimpleVectorIndex(documents, llm_predictor=llm_predictor, prompt_helper=prompt_helper)
+
+    index.save_to_disk('index.json')
+
+    return index
+
+
+def greenify(input):
+    return "\x1b[102m" + input + "\x1b[0m"
+
+def call_gpt(cur_prompt, stop):
+    ans = openai.Completion.create(
+        model="text-davinci-003",
+        max_tokens=256,
+        stop=stop,
+        prompt=cur_prompt,
+        temperature=0)
+    returned = ans['choices'][0]['text']
+    print(greenify(returned), end='')
+
+    return returned
+
+def get_answer(query):
+    response = index.query(query, response_mode="compact")
+    print(response)
+    print("\nBot says: \n\n" + response.response + "\n\n\n")
+    query_int = input("\nWhether this answer fit your question?y/n: \n")
+    return query_int
+
+def get_last_line(generated):
+    if '\n' not in generated:
+        last_line =  generated
+    else:
+        last_line = generated.split('\n')[-1]
+
+
+    return last_line
+
+
+def extract_question(generated):
+    if '\n' not in generated:
+        last_line = generated
+    else:
+        last_line = generated.split('\n')[-1]
+
+    if 'Follow up:' not in last_line:
+        print('we probably should never get here...' + generated)
+
+    if ':' not in last_line:
+        after_colon = last_line
+    else:
+        after_colon = generated.split(':')[-1]
+
+    if ' ' == after_colon[0]:
+        after_colon = after_colon[1:]
+    if '?' != after_colon[-1]:
+        print('we probably should never get here...' + generated)
+
+    return after_colon
+
+def promptf(question, prompt, intermediate="\nIntermediate answer:", followup="Follow up:",
+            finalans='\nSo the final answer is:'):
+    cur_prompt = prompt[0] + question + prompt[1]
+
+    print(cur_prompt, end='')
+
+    ret_text = call_gpt(cur_prompt, intermediate)
+    count = 0
+    while followup in get_last_line(ret_text):
+
+        cur_prompt += ret_text
+        question = extract_question(ret_text)
+        external_answer = get_answer(question)
+
+        if external_answer=='y':
+            print('======Move to the next question=======')
+
+            return external_answer
+        else:
+            print('We try to find another intermediate question for you\n')
+            # We only get here in the very rare case that Google returns no answer.
+            cur_prompt += intermediate
+            print(intermediate + ' ')
+            ret_text = call_gpt(cur_prompt, ['\n' + followup, finalans])
+            #cur_prompt += gpt_answer
+           # external_answer = get_answer(question)
+        count+=1
+       # print(count,'----------')
+        if count==10:
+            return external_answer
+
+
+
+def ask_bot(input_index = 'index.json'):
+  index = GPTSimpleVectorIndex.load_from_disk(input_index)
+  total_q = 0
+  while True and total_q<10:
+    query = input('What do you want to ask the bot?   \n')
+    if query == 'quit':
+        return 0
+    else:
+
+        response = index.query(query, response_mode="compact")
+        print(response)
+        print ("\nBot says: \n\n" + response.response + "\n\n\n")
+        query_int = input("\nWhether this answere fit your question?y/n: \n")
+        if query_int=='y':
+            print('======Move to the next question=======')
+        elif query_int=='n':
+
+            print('We try to find a intermediate question for you\n')
+            res = promptf(query,prompt)
+            if res=='n':
+                print('======Limited. Move to the next question=======')
+            else:
+                print('======Move to the next question=======')
+        total_q+=1
+
+
+
+if __name__ == "__main__":
+
+    test_urls= ['https://en.wikipedia.org/wiki/Barack_Obama',
+                'https://en.wikipedia.org/wiki/Harry_Potter',
+                'https://en.wikipedia.org/wiki/Elon_Musk',
+                'https://en.wikipedia.org/wiki/Vincent_van_Gogh',
+                'https://en.wikipedia.org/wiki/Roger_Federer'
+                ]
+    for test_url in test_urls:
+        text = html2arti(test_url)
+        text = repeat_keyword(text)
+        text_name = test_url.split('/')[-1]
+        text_path = './txts_'+text_name
+        if not os.path.exists(text_path):
+            os.mkdir(text_path)
+        content_path = text_path+'/content.txt'
+        with open(content_path, "a") as f:
+            f.write(text)
+        index = construct_index(text_path)
+        ask_bot('index.json')
---
+++ html2text.py
@@ -0,0 +1,59 @@
+from newspaper import Article
+
+# print('--------------------------------')
+# url = 'https://en.wikipedia.org/wiki/Bug_(comics)'#https://en.wikipedia.org/wiki/Barack_Obama'
+# article = Article(url)
+# article.download()
+# article.parse()
+# print(article.text)
+
+from typing import List
+
+
+
+def repeat_keyword(text):
+    new_text = ""
+    key_list = []
+    count = 0
+    j = 0
+
+    for line in text.splitlines():
+        print('------------',line,'--------------')
+        if len(line)<20 and len(line)>5:
+            key_list+=[line]
+            j+=1
+
+        else:
+            if len(key_list) and len(line)>5:
+                new_text+=key_list[j-1]+' : '+line
+                if line[-1]=='.':
+                    new_text+='\n'#'#+'\n'
+            else:
+                new_text+=line
+                if len(line) and line[-1] == '.':
+                    new_text += '\n'  # '#+'\n'
+                #+'\n'
+
+    print(new_text)
+    print('======new======')
+
+    return new_text
+
+
+
+def html2arti(url):
+    #"url = 'https://en.wikipedia.org/wiki/Barack_Obama'
+    article = Article(url)
+    article.download()
+    article.parse()
+    return article.text
+
+if __name__ == "__main__":
+
+    test_url= 'https://en.wikipedia.org/wiki/Barack_Obama'
+
+    text = html2arti(test_url)
+    text = repeat_keyword(text)
+    text_path = 'obama_wiki'
+    with open(text_path, "a") as f:
+        f.write(text)
